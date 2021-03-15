8aef73c51199cb628efd1e7e6971c08311b344e8
Vinicio Silva <viniciomsilva@outlook.com.br>
2021-03-14 21:26:08 -0300

    Prettier

--- src/index.js
+++ src/index.js
@@ -69,8 +69,8 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
   const { user } = request;
-  const { title, deadline } = request.body;
   const { id } = request.params;
+  const { title, deadline } = request.body;
 
   const todo = user.todos.find((todo) => todo.id === id);
 
